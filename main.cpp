bad11416584674f2fe0a0df78f3a59e551c496c5
wszy5 <wikoszyman@wp.pl>
2022-04-29 01:16:01 +0200

    clarified code and wrote isClicked function

--- main.cpp
+++ main.cpp
@@ -2,109 +2,154 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+
+class CustomRectangleShape : public sf::RectangleShape {
+public:
+    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position) : sf::RectangleShape(size)
+    {
+        setPosition(position);
+
+    }
+
+    void setSpeed(const int& x_speed, const int& y_speed, const int & ro_speed) {
+        x_speed_ = x_speed;
+        y_speed_ = y_speed;
+        ro_speed_ = ro_speed;
+    }
+    void animate(const sf::Time &elapsed){
+        bouncce();
+        float dt = elapsed.asMicroseconds();
+        move(x_speed_*dt,y_speed_*dt);
+        rotate(ro_speed_*dt);
+    }
+    void setBounds(const float& l_bound, const float& r_bound,const float& u_bound,const float& d_bound){
+        l_bound_  = l_bound  ;
+        r_bound_  = r_bound  ;
+        u_bound_  = u_bound  ;
+        d_bound_  = d_bound  ;
+    }
+    void moveInDirection(const sf::Time &elapsed, const sf::Keyboard::Key &key)
+    {
+        float dt = elapsed.asSeconds();
+        if (key == sf::Keyboard::Left)
+        {
+           bouncce();
+          move(-abs(x_speed_)*dt,0);
+        }
+        else if (key == sf::Keyboard::Right)
+        {
+            bouncce();
+          move(abs(x_speed_)*dt,0);
+        }
+        else if (key == sf::Keyboard::Up){
+            bouncce();
+            move(0, -abs(y_speed_)*dt);
+        }
+        else if (key == sf::Keyboard::Down){
+            bouncce();
+            move(0, abs(y_speed_)*dt);
+        }
+    }
+
+    bool isClicked(sf::Vector2i &mouse_position, sf::Vector2f &rec_position) const{
+
+        sf::FloatRect rectangle_bounds = getGlobalBounds();
+        float xrange = rectangle_bounds.width/2;
+        float yrange = rectangle_bounds.height/2;
+        if((mouse_position.x>= rec_position.x-xrange && mouse_position.x<= rec_position.x+xrange) &&
+                (mouse_position.y>= rec_position.y-yrange && mouse_position.y<= rec_position.y+yrange))
+        {
+            return true;
+        }
+
+               return false;
+    }
+
+private:
+    int x_speed_ = 0 ;
+    int y_speed_ = 0 ;
+    int ro_speed_ = 0 ;
+    float l_bound_ = 0;
+    float r_bound_ = 0;
+    float u_bound_ = 0;
+    float d_bound_ = 0;
+
+
+    void bouncce(){
+        sf::FloatRect rectangle_bounds = getGlobalBounds();
+
+        if(rectangle_bounds.top <= u_bound_){
+            y_speed_ = abs(y_speed_);
+        }
+
+        if(rectangle_bounds.top + rectangle_bounds.height >= d_bound_){
+            y_speed_ = abs(y_speed_) * -1;
+            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+        }
+
+        if(rectangle_bounds.left <= l_bound_ ){
+           x_speed_ = abs(x_speed_);
+           setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+        }
+
+        if(rectangle_bounds.left + rectangle_bounds.width >= r_bound_){
+            x_speed_ = abs(x_speed_) * -1;
+            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
+        }
+    }
+
+
+};
+
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "main");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    sf::CircleShape circle(50.0);
-    circle.setPosition(100.0, 300.0);
-    circle.setFillColor(sf::Color(100, 250, 50));
-
-    sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
-
-    sf::ConvexShape triangle;
-    triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
-    triangle.setPoint(1, sf::Vector2f(0.0, 100.0));
-    triangle.setPoint(2, sf::Vector2f(140.0, 40.0));
-    triangle.setOutlineColor(sf::Color::Red);
-    triangle.setOutlineThickness(2);
-    triangle.setPosition(600.0, 100.0);
+    sf::Vector2f size(120.0, 60.0);
+    sf::Vector2f position(120.0, 60.0);
+    CustomRectangleShape rectangle_inh(size, position);
+    rectangle_inh.setFillColor(sf::Color(150, 100, 50));
+    rectangle_inh.setSpeed(100, 150, 10);
 
     sf::Clock clock;
-
-    int rectangle_velocity_x = 70;
-    int rectangle_velocity_y = 100;
-    int rectangle_angular_velocity = 80;
-
-    bool flag_y = false;
-    bool flag_x = false;
-
-    int margin = 10;
+    // run the program as long as the window is open
     while (window.isOpen()) {
+
         sf::Time elapsed = clock.restart();
-        float dt = elapsed.asSeconds();
-        sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
 
-        rectangle.move(rectangle_velocity_x*dt,rectangle_velocity_y*dt);
-        if((rectangle_bounds.top>0+margin && rectangle_bounds.left>0+margin) && (rectangle_bounds.top<800-margin && rectangle_bounds.left<600-margin))
-        {
+        rectangle_inh.setBounds(0, window.getSize().x, 0, window.getSize().y);
 
-        rectangle.rotate(rectangle_angular_velocity*dt);
-}
-//        sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
-        std::cout << rectangle_bounds.top << " " << rectangle_bounds.left << " " ;
-        std::cout << rectangle_bounds.width << " " << rectangle_bounds.height << std::endl;
-
-        if(rectangle_bounds.top-margin<=0 || rectangle_bounds.top+rectangle_bounds.height>=window.getSize().y)
-            {
-                if(flag_y != true)
-                {
-                    rectangle_velocity_y *= -1;
-                    rectangle.setFillColor(sf::Color(rand() % 256,
-                                                     rand() % 256,
-                                                     rand() % 256));
-                }
-                flag_y = true;
-            }
-            else
-                flag_y = false;
-
-
-
-            if(rectangle_bounds.left-margin<=0 || rectangle_bounds.left+rectangle_bounds.width>=window.getSize().x)
-            {
-                if(flag_x!=true)
-                {
-                    rectangle_velocity_x *= -1;
-                    rectangle.setFillColor(sf::Color(rand() % 256,
-                                                     rand() % 256,
-                                                     rand() % 256));
-                }
-                flag_x = true;
-            }
-            else
-                flag_x = false;
-
-//            if(rectangle_bounds.left<=0){
-//                rectangle_velocity_x *= -1;
-//                flag_x = false;
-////
-////                rectangle.rotate(rectangle_angular_velocity*dt);
-//                rectangle.setFillColor(sf::Color(rand() % 256,
-//                                                 rand() % 256,
-//                                                 rand() % 256));
-
-//            }
-
-
-
-
+         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
 
         while (window.pollEvent(event)) {
-
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        sf::Vector2i mouse_position;
+        if(event.type == sf::Event::KeyPressed){
+            rectangle_inh.moveInDirection(elapsed, event.key.code);
+        }
+        else if(event.type == sf::Event::MouseButtonPressed){
+            if(event.mouseButton.button == sf::Mouse::Left){
+                mouse_position = sf::Mouse::getPosition(window);
+            }
+        }
+ auto pos = rectangle_inh.getPosition();
+        if(rectangle_inh.isClicked(mouse_position,pos)){
+            rectangle_inh.setFillColor(sf::Color(rand() % 256,rand() % 256,rand() % 256));
+        }
+
+
+//
+        // clear the window with black color
         window.clear(sf::Color::Black);
+        // draw everything here...
 
-        window.draw(circle);
-        window.draw(rectangle);
-        window.draw(triangle);
-
+        window.draw(rectangle_inh);
+        // end the current frame
         window.display();
     }
 
